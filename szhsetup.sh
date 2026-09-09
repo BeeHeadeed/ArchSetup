d075f8e2286a911e2661993560e2ef51550fe29c
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-09 14:37:43 +0200

    add: mkdir

--- szhsetup.sh
+++ szhsetup.sh
@@ -1,4 +1,5 @@
 git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git ~/tmp/pokemon-colorscripts
+mkdir ~/pok-bin
 chmod 777 -R ~/tmp/
 ./custom_pok_install.sh
 rm -rf ~/tmp/pokemon-colorscripts
