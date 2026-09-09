7b585842aa00a1669055f91ffae74011b974e4a1
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-09 15:01:06 +0200

    change: p10k config

--- copy.sh
+++
@@ -1,6 +0,0 @@
-# copy .zshrc
-cp .zshrc ~/
-
-# copy Hyde and Hypr config folders
-cp -r hyde ~/.config/
-cp -r hypr ~/.config/
--- szhsetup.sh
+++ szhsetup.sh
@@ -1,10 +1,8 @@
-git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git ~/tmp/pokemon-colorscripts
-mkdir ~/pok-bin
-chmod 777 -R ~/tmp/
 ./custom_pok_install.sh
 rm -rf ~/tmp/pokemon-colorscripts
 
 apt-get eza
 
 echo "tamere"
+cp ./.p10k.szh "${HOME}"
 cp ./.zshrc "${HOME}"
