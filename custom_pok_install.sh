516866a9893ce38dab77e7d5f299a15bdbdf3108
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-09 14:28:30 +0200

    change: pok-install

---
+++ custom_pok_install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# A basic install script for pokemon-colorscripts
+
+INSTALL_DIR='/pok'
+BIN_DIR='/pok-bin'
+DIR_PATH= /tmp/pokemon-colorscripts
+
+export PATH="$BIN_DIR:$PATH"
+
+# deleting directory if it already exists
+rm -rf "$INSTALL_DIR/pokemon-colorscripts" || return 1
+
+# making the necessary folder structure
+mkdir -p $INSTALL_DIR/pokemon-colorscripts || return 1
+
+# moving all the files to appropriate locations
+cp -rf $DIR_PATH/colorscripts $INSTALL_DIR/pokemon-colorscripts
+cp $DIR_PATH/pokemon-colorscripts.py $INSTALL_DIR/pokemon-colorscripts
+cp $DIR_PATH/pokemon.json $INSTALL_DIR/pokemon-colorscripts
+
+# create symlink in usr/bin
+rm -rf "$BIN_DIR/pokemon-colorscripts" || return 1
+ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.py $BIN_DIR/pokemon-colorscripts
+
--- szhsetup.sh
+++ szhsetup.sh
@@ -1,8 +1,5 @@
-cp -r ./zsh-theme-powerlevel10k/ /usr/share/zsh-theme-powerlevel10k/
-cp -r ./oh-my-zsh/ /usr/share/oh-my-zsh/
-
 git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git /tmp/pokemon-colorscripts
-/tmp/pokemon-colorscripts/install.sh
+./custom_pok_install.sh
 rm -rf /tmp/pokemon-colorscripts
 
 apt-get eza
