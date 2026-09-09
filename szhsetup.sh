5d36f55734dc1db8e7c182af37736030570d2197
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-09 14:14:01 +0200

    fix: cp -r

--- szhsetup.sh
+++ szhsetup.sh
@@ -1,9 +1,9 @@
-cp ./zsh-theme-powerlevel10k/ /usr/share/zsh-theme-powerlevel10k/
-cp ./usr/share/oh-my-zsh/ /usr/share/oh-my-zsh/
+cp -r ./zsh-theme-powerlevel10k/ /usr/share/zsh-theme-powerlevel10k/
+cp -r ./oh-my-zsh/ /usr/share/oh-my-zsh/
 
 git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git /tmp/pokemon-colorscripts
 /tmp/pokemon-colorscripts/rinstall.sh
-rm -rf /tmp/pokemon-colorscripts
+# rm -rf /tmp/pokemon-colorscripts
 
 apt-get eza
 
