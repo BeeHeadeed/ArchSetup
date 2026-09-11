f62d4d9243591252d9602ed9860d289f60e76867
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-11 11:44:17 +0200

    test

--- custom_pok_install.sh
+++ custom_pok_install.sh
@@ -13,6 +13,7 @@ mkdir $BIN_DIR
 chmod 777 -R ${HOME}/tmp/
 
 # export PATH="$BIN_DIR:$PATH"
+export PATH="$BIN_DIR:$PATH"
 export PATH="$INSTALL_DIR:$PATH"
 
 # deleting directory if it already exists
@@ -30,3 +31,4 @@ cp $DIR_PATH/pokemon.json $INSTALL_DIR/pokemon-colorscripts
 rm -rf "$BIN_DIR/pokemon-colorscripts" || return 1
 cp ./pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts/
 ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $BIN_DIR/pokemon-colorscripts
+ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts
