525f64c3bc91dac756642fd6a68dd2c6b2444178
Quentin Leguay <quentinleguay40@gmail.com>
2026-09-24 15:21:33 +0200

    add: paths

--- Dotfiles/custom_pok_install.sh
+++ Dotfiles/custom_pok_install.sh
@@ -27,6 +27,6 @@ cp $DIR_PATH/pokemon.json $INSTALL_DIR/pokemon-colorscripts
 
 # create symlink in usr/bin
 rm -rf $BIN_DIR/pokemon-colorscripts || return 1
-mv ./pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts/
+mv ./Dotfiles/pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts/
 ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $BIN_DIR/pokemon-colorscripts
 ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts
